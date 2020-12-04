66f317ad73358184951b3705ac608386b0de71d2
katie-codes <katiepattersondesign@gmail.com>
2020-12-04 10:37:53 +1000

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Footer.css";
-import mainTick from "../assets/opt-in_favicon.jpg";
 import fb from "../assets/opt-in_fb_white.png";
 
 
@@ -9,8 +8,6 @@ function Footer() {
 
     return (
         <div className="my-footer">
-            <Link to={'/'}>
-            <img className="footer-tick" src={mainTick}/> </Link>
             <h2 className="footer">2020 Booty Boolean Beyonces</h2>
             <img className="footer-fb" src={fb}/>    
         </div>
