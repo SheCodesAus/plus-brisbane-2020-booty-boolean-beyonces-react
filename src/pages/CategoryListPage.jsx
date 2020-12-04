bb3270116977de881856b3f993fc407b4744bad7
Albany276 <albany.alfonzo@hotmail.com>
2020-12-05 09:17:49 +1000

    last minute changes cat page

--- src/pages/CategoryListPage.jsx
+++ src/pages/CategoryListPage.jsx
@@ -6,13 +6,6 @@ import "../App.css";
 
 
 
-function CategoryListPage () {
-
-
-    const category = useParams();
-    const [categoryList, setCategoryList] = useState([]);
-    const token = window.localStorage.getItem("token")
-
 
 
 // AA 02.12: needed to define 2 different fetches, one for when you are logged in and you pass the token and 
@@ -23,7 +16,7 @@ function CategoryListPage () {
 // notloggedinfetch is used
 
 
-    function Loggedfetch(category, categoryList, setCategoryList){
+    function Loggedfetch(token, category, categoryList, setCategoryList){
 
 
     useEffect(() => {
@@ -41,10 +34,10 @@ function CategoryListPage () {
             // console.log(categoryList)
         });
     }, []);
-}
+    	}
 
 
-function Notloggedfetch(category, categoryList, setCategoryList) {
+    function Notloggedfetch(category, categoryList, setCategoryList) {
 
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/products/${category.category}`)
@@ -57,64 +50,17 @@ function Notloggedfetch(category, categoryList, setCategoryList) {
         });
     }, []);
 
-}
+    }
 
-function Categorydealer(category){
-    // var categoryDict = {
-    //     "laptop": [`From the boardroom to the beach, laptops are more powerful than ever and enabling employees to work from home with ease. Whether you’re a spreadsheet whiz or a creative champion, we’ve curated the top-notch options on the market today. `,
-    //             `Our experts say… “The soon-to-be released MacBook Air is quickly catching up to the Pro model, now boasting a much faster CPU and Apple’s brand-spanking-new M1 processor. While it doesn’t quite have the same power as the Pro, MacBook Air is a fantastic option for those who want the creative capabilities of Apple with a slightly more reasonable price tag.”`],
-    //     "phone": [`From counting our steps to actually, well, calling people - where would we be in today’s connected world without our phones? We’d pay a pretty penny to hear Alexander Graham Bell’s thoughts on how his original invention has evolved over the years.`,
-    //                 `Our experts say… “While the new iPhone looks flashy and admittedly, has an outstanding camera, it’s lacking the USB-C port that most other Android models have adapted. If you’re not a die-hard Apple fan, we recommend checking out the new Google Pixel 5.”`],
-    //     "tablet": [`For when laptops are too big and phones are too small - enter the tablet. If you’re goldilocks looking for something that’s just right, our selection might just have that perfect bowl of porridge.`,
-    //             `Our experts say… “The new iPad Air is impressively powerful, boasting many of the same features that attract customers to the Pro version.  Even better, you can choose from a range of colours. Match your outfit, your mood, or even your dog’s favourite toy.”`],
-    //     "accessory": [`Pimp my tech - here are the latest and greatest accessories designed to help optimise your laptop, phone or tablet. From WFH essentials such as the bluetooth mouse to a stylish device cover, our selection passes the vibe check.`,]
-    // };
-
-    // const displayCat = () => {
-    //     if (category.category == "laptop") {
-    //         return "Laptops"
-    //     } else if (category.category == "phone") {
-    //         return "Phones"
-    //     } else if (category.category == "tablet") {
-    //         return "Tablets"
-    //     }
-    //     return "Accessories"
-    // }
-
-    // const displayDesc1 = () => {
-    //     return categoryDict[category.category][0]
-    // }
-
-    // const displayDesc2 = () => {
-    //     return categoryDict[category.category][1]
-    // }
-
-    // const thisCat = displayCat()
-    // const descPartOne = displayDesc1()
-    // const descPartTwo = displayDesc2()
-
-
-
-}
 
 function CategoryListPage () {
+
+
     const category = useParams();
     const [categoryList, setCategoryList] = useState([]);
-    // const category = useParams();
-    // const category = props;
-    // const [categoryList, setCategoryList] = useState([]);
-    // const token = window.localStorage.getItem("token")
- 
+    const token = window.localStorage.getItem("token")
+    
 
-    if (token){
-        Loggedfetch(category, categoryList,setCategoryList)
-    }
-
-    else {
-        Notloggedfetch(category, categoryList, setCategoryList)
-    }
-
-    // Categorydealer(category)
     var categoryDict = {
         "laptop": [`From the boardroom to the beach, laptops are more powerful than ever and enabling employees to work from home with ease. Whether you’re a spreadsheet whiz or a creative champion, we’ve curated the top-notch options on the market today. `,
                 `Our experts say… “The soon-to-be released MacBook Air is quickly catching up to the Pro model, now boasting a much faster CPU and Apple’s brand-spanking-new M1 processor. While it doesn’t quite have the same power as the Pro, MacBook Air is a fantastic option for those who want the creative capabilities of Apple with a slightly more reasonable price tag.”`],
@@ -150,7 +96,7 @@ function CategoryListPage () {
 
 
     if (token){
-        Loggedfetch(category, categoryList,setCategoryList)
+        Loggedfetch(token, category, categoryList,setCategoryList)
     }
 
     else {
@@ -167,7 +113,7 @@ function CategoryListPage () {
             </div>
                 <div id="product-list">
                 {categoryList.map((productData, key) => {
-                    return <ProductCard key={key} productData={productData} />;
+                    return <ProductCard key={key} productData={productData} />
                 })}
             </div>
         </div>
